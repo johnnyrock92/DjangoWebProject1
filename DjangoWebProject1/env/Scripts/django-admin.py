cd69f5fef8771c6398ead10ed930c8161d9bae02
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-05-30 16:55:47 +0200

    testy

---
+++ DjangoWebProject1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Janusz\Projekty_w_Visual_Studio_2015\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
